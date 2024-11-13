f9d8f21d00051d6bfef3e3633caaad1eb3062a3e
Robert Hayek <hayekr2@gmail.com>
2024-11-12 21:33:50 -0600

    Output model summary

---
+++ get_model_summary.py
@@ -0,0 +1,12 @@
+from click.core import batch
+from torchinfo import summary
+
+from task import Net
+
+model = Net()
+batch_size = 16
+depth = 3
+height, width = 32, 32
+summary(model, input_size=(batch_size, depth, height, width), device='cpu', col_names=["kernel_size", "output_size", "num_params"],
+    row_settings=["var_names"])
+print(model)
