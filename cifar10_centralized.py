d2a5ee63040049b3ac6fbb3516deae6d59fca8a9
Robert Hayek <hayekr2@gmail.com>
2024-11-18 23:02:42 -0600

    Revert "Make centralized better"
    This reverts commit 5f10f3c1

--- cifar10_centralized.py
+++ cifar10_centralized.py
@@ -4,133 +4,75 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 from task import Net
 import torch.nn as nn
-import logging
-from typing import List
-logging.getLogger().setLevel(logging.INFO)
-logger = logging.getLogger(__name__)
 
-class Centralized:
-    def __init__(self, batch_size=16, learning_rate=0.001, momentum=0.9, val_split=0.2):
-        self.device = 'cpu'
-        self.batch_size = batch_size
-        self.learning_rate = learning_rate
-        self.momentum = momentum
 
-        self.classes = ('plane', 'car', 'bird', 'cat',
-                   'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-        self.net = None
-        self.criterion = None
-        self.optimizer = None
-        self.trainloader = None
-        self.valloader = None
-        self.val_split = val_split
-        # Track losses
-        self.train_losses: List[float] = []
-        self.val_losses: List[float] = []
-    def setup_data(self):
-        """Set up data transformations and dataloaders with validation split"""
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+batch_size = 4
 
-        full_trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                download=True, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
 
-        # Calculate split sizes
-        val_size = int(len(full_trainset) * self.val_split)
-        train_size = len(full_trainset) - val_size
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
 
-        # Split into train and validation sets
-        trainset, valset = torch.utils.data.random_split(
-            full_trainset,
-            [train_size, val_size]
-        )
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-        # Create data loaders
-        self.trainloader = torch.utils.data.DataLoader(
-            trainset,
-            batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=2
-        )
+net = Net()
 
-        self.valloader = torch.utils.data.DataLoader(
-            valset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=2
-        )
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    def setup_model(self):
-        self.net = Net().to(self.device)
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.SGD(self.net.parameters(), lr=self.learning_rate, momentum=self.momentum)
 
-    def train(self, num_epochs):
-        logger.info('Training for {} epochs'.format(num_epochs))
-        for epoch in range(num_epochs):  # loop over the dataset multiple times
+for epoch in range(1):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0
-            for i, data in enumerate(self.trainloader, 0):
-                inputs, labels = data
-                # zero the parameter gradients
-                self.optimizer.zero_grad()
 
-                # forward + backward + optimize
-                outputs = self.net(inputs)
-                loss = self.criterion(outputs, labels)
-                loss.backward()
-                self.optimizer.step()
-
-                # print statistics
-                running_loss += loss.item()
-                if i % 2000 == 1999:  # print every 2000 mini-batches
-                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                    running_loss = 0.0
-
-        logger.info('Finished Training')
-
-    def save_model(self, path='./cifar_net.pth'):
-        torch.save(self.net.state_dict(), path)
-
-    def test(self):
-        if self.net is None:
-            raise Exception('Model has not been trained yet')
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for data in self.valloader:
-                images, labels = data
-                images, labels = images.to(self.device), labels.to(self.device)
-                outputs = self.net(images)
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-
-        accuracy = 100 * correct // total
-        print(f'Accuracy of the network on the 10000 test images: {accuracy}%')
-        return accuracy
+print('Finished Training')
 
 
-def main():
-    # Create trainer instance
-    trainer = Centralized(batch_size=16, val_split=0.2)
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
 
-    # Setup
-    trainer.setup_data()
-    trainer.setup_model()
+dataiter = iter(testloader)
+images, labels = next(dataiter)
 
-    # Train and get history
-    trainer.train(num_epochs=100)
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-    # Save the model and history
-    trainer.save_model()
-
-    # Evaluate
-    trainer.test()
-
-
-if __name__ == "__main__":
-    main()
-
+print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
