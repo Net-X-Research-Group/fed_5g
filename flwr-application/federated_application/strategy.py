de4b33c0bf8b49b21fa238799da16ad87153d5f4
Robert Hayek <hayekr2@gmail.com>
2024-11-25 18:53:09 -0600

    Output after final round, save as json. Optimize imports

--- flwr-application/federated_application/strategy.py
+++ flwr-application/federated_application/strategy.py
@@ -1,8 +1,10 @@
 import json
 import logging
+import os
+
 from flwr.common.typing import UserConfig
 from flwr.server.strategy import FedAvg
-import os
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -13,25 +15,20 @@ logger = logging.getLogger(__name__)
 class MetricsFedAvg(FedAvg):
     def __init__(self, run_config: UserConfig, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.indiv_training_time = []
-        self.indiv_fit_time = []
-        self.results = {}
+        self.results = dict()
+        self.num_rounds = run_config['rounds']
 
-    def _store_results(self, results_dict):
-        print('SAVING THE STUFF')
-        with open(f"{os.path.expanduser('~/results.json')}", "w", encoding="utf-8") as f:
-            json.dump(results_dict, f)
+    def _log_results(self, server_round, results):
+        self.results[server_round] = results
 
+        if server_round == self.num_rounds:
+            with open(f"{os.path.expanduser('~/results.json')}", "w") as f:
+                json.dump(self.results, f)
 
     # Define metric aggregation function
     def aggregate_fit(self, server_round, results, failures):
         params, metrics = super().aggregate_fit(server_round, results, failures)
-
-        # Store results and log
-        if metrics:
-            self._store_results(metrics)
-
+        self._log_results(server_round, metrics)
         return params, metrics
-
 
 
