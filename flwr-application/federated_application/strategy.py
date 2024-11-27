252c82e95571184d9bc80b1bc17daaa243526445
Robert Hayek <hayekr2@gmail.com>
2024-11-27 00:20:40 -0600

    Remove api key from project name, redo run name

--- flwr-application/federated_application/strategy.py
+++ flwr-application/federated_application/strategy.py
@@ -20,13 +20,13 @@ class MetricsFedAvg(FedAvg):
     def __init__(self, run_config: UserConfig, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.config = run_config
+        self.config.pop('wandb_api_key')
         self.num_rounds = run_config['rounds']
         self.epochs = run_config['local_epochs']
         self.clients = run_config['min_num_clients']
         self.batch_size = run_config['batch_size']
         # Login to wandb using API key.
         wandb.login(key=run_config['wandb_api_key'])
-
         # Initialize W&B project
         self._init_wandb_project()
 
@@ -34,7 +34,7 @@ class MetricsFedAvg(FedAvg):
         self.results = {}
 
     def _init_wandb_project(self):
-        wandb.init(project=PROJECT_NAME, name=f'{str(self.config)}-ServerApp', config=self.config)
+        wandb.init(project=PROJECT_NAME, name=f'{self.clients}C.{self.epochs}E.{self.batch_size}B.{self.num_rounds}R-ServerApp', config=self.config)
 
     def _log_results(self, server_round, results):
         self.results[server_round] = results
