e6662f61752d8bbb646b853014c0bc8681fc49f6
Robert Hayek <hayekr2@gmail.com>
2024-11-29 14:56:37 -0500

    Call init every time,

--- flwr-application/federated_application/mods.py
+++ flwr-application/federated_application/mods.py
@@ -11,30 +11,29 @@ PROJECT_NAME = "Pytorch-5G-FLWR-CIFAR10"
 
 def wandb_metrics_mod(message: Message, context: Context, app: ClientAppCallable) -> Message:
     current_round = int(message.metadata.group_id)
-    if current_round == 1 and message.metadata.message_type == MessageType.TRAIN:
-        # Authenticate with wandb
-        wandb.login(key=context.run_config['wandb_api_key'])
+    # Authenticate with wandb
+    wandb.login(key=context.run_config['wandb_api_key'])
 
-        # Initialize the wandb project
-        run_id = message.metadata.run_id
-        group_name = f'Run ID: {run_id}'
-        node_id = context.node_config['cid']
-        run_name = f'{datetime.now().strftime("%Y-%m-%d/%H-%M-%S")}_CID-{node_id}'
-        wandb.init(
-            project=PROJECT_NAME,
-            group=group_name,
-            name=run_name,
-            id=f'{run_id}-{node_id}',
-            resume='allow',
-            reinit=True,
-            config={'rounds': context.run_config['rounds'],
-                    'fraction_evaluate': context.run_config['fraction_evaluate'],
-                    'local_epochs': context.run_config['local_epochs'],
-                    'learning_rate': context.run_config['learning_rate'],
-                    'batch_size': context.run_config['batch_size'],
-                    'min_num_clients': context.run_config['min_num_clients']
-                    }
-        )
+    # Initialize the wandb project
+    run_id = message.metadata.run_id
+    group_name = f'Run ID: {run_id}'
+    node_id = context.node_config['cid']
+    run_name = f'{datetime.now().strftime("%Y-%m-%d/%H-%M-%S")}_CID-{node_id}'
+    wandb.init(
+        project=PROJECT_NAME,
+        group=group_name,
+        name=run_name,
+        id=f'{run_id}-{node_id}',
+        resume='allow',
+        reinit=True,
+        config={'rounds': context.run_config['rounds'],
+                'fraction_evaluate': context.run_config['fraction_evaluate'],
+                'local_epochs': context.run_config['local_epochs'],
+                'learning_rate': context.run_config['learning_rate'],
+                'batch_size': context.run_config['batch_size'],
+                'min_num_clients': context.run_config['min_num_clients']
+                }
+    )
 
     start = time.time()
 
