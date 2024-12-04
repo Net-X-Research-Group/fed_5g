20df84a6f3c38a6efe3e5ee25e4416778ff15824
Robert Hayek <hayekr2@gmail.com>
2024-12-04 00:12:21 -0600

    Working mod to get comms closer to tx/rx

--- flwr-application/federated_application/mods.py
+++ flwr-application/federated_application/mods.py
@@ -19,14 +19,13 @@ logger = logging.getLogger(__name__)
 PROJECT_NAME = "Pytorch-5G-FLWR-CIFAR10"
 
 def uplink_time_mod(message: Message, context: Context, app: ClientAppCallable) -> Message:
+    downlink_time_server = message.content.configs_records['fitins.config']['server_timestamp']
+    message.content.configs_records['fitins.config']['downlink_time'] = time.time() - downlink_time_server
     reply = app(message, context)
-    if reply.metadata.message_type == MessageType.TRAIN and reply.has_content():
-        metrics = reply.content.configs_records
-        logged_results = dict(metrics.get('fitres.metrics', ConfigsRecord()))
-        logger.info('The logged results are: %s', logged_results)
-        #logged_results['uplink_time'] = time.time()
-        #reply.content.configs_records['fitres.metrics'] = logged_results['']
+    if reply.metadata.message_type == MessageType.TRAIN:
+        reply.content.configs_records['fitres.metrics']['uplink_time'] = time.time()
     return reply
+
 
 def wandb_metrics_mod(message: Message, context: Context, app: ClientAppCallable) -> Message:
     current_round = int(message.metadata.group_id)
