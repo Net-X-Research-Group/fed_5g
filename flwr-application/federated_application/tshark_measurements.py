7557f147f670f9e0cc70240af145d9f2f05e1f47
Robert Hayek <hayekr2@gmail.com>
2024-12-28 17:48:16 -0500

    Add comma

--- flwr-application/federated_application/tshark_measurements.py
+++ flwr-application/federated_application/tshark_measurements.py
@@ -18,7 +18,7 @@ def start_tshark(output_file):
     tshark_cmd = [
         'tshark',
         '-i', 'oai-cn5g',  # Attach to OAI CN interface
-        '-f', '\"tcp port 9092\"'
+        '-f', '\"tcp port 9092\"',
         '-w', os.path.expanduser(f'~/{output_file}.pcapng')
     ]
     return subprocess.Popen(tshark_cmd)
