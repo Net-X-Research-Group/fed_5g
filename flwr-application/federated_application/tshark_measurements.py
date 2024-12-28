d7092f9a10b59df76d73b7e9c366f542237fbb74
Robert Hayek <hayekr2@gmail.com>
2024-12-28 18:11:21 -0500

    Fix tshark command

--- flwr-application/federated_application/tshark_measurements.py
+++ flwr-application/federated_application/tshark_measurements.py
@@ -3,22 +3,11 @@ import subprocess
 import logging
 from datetime import datetime
 
-def _ensure_tshark():
-    try:
-        subprocess.run(['tshark', '-v'], check=True)
-    except FileNotFoundError:
-        logging.error("tshark not found. Please install Wireshark.")
-        exit(1)
-    except subprocess.CalledProcessError:
-        logging.error("tshark not found. Please install Wireshark.")
-        exit(1)
-
 def start_tshark(output_file):
-    _ensure_tshark()
     tshark_cmd = [
         'tshark',
         '-i', 'oai-cn5g',  # Attach to OAI CN interface
-        '-f', '\"tcp port 9092\"',
+        '-f', 'tcp port 9092',
         '-w', os.path.expanduser(f'~/{output_file}.pcapng')
     ]
     return subprocess.Popen(tshark_cmd)
