b3b45907bef6d910cac7860a61a8df5fc3d9a13e
Robert Hayek <hayekr2@gmail.com>
2025-01-24 12:58:07 -0600

    add flag for cellular, fix termination of process.

--- flwr-application/federated_application/tshark_measurements.py
+++ flwr-application/federated_application/tshark_measurements.py
@@ -3,17 +3,29 @@ import subprocess
 import logging
 from datetime import datetime
 
-def start_tshark(output_directory):
-    tshark_cmd = [
-        'tshark',
-        '-n',
-        '-i', 'oai-cn5g',  # Attach to OAI CN interface
-        '-w', os.path.expanduser(f'~/{output_directory}/output.pcapng')
-    ]
-    return subprocess.Popen(tshark_cmd)
+CELLULAR = False
+
+def start_tshark(output_directory) -> Popen[bytes]:
+    if CELLULAR:
+        tshark_cmd = [
+            'tshark',
+            '-n',
+            '-i', 'oai-cn5g',  # Attach to OAI CN interface
+            '-w', os.path.expanduser(f'~/{output_directory}/output.pcapng')
+        ]
+    else:
+        tshark_cmd = [
+            'tshark',
+            'f', 'tcp port 9092',
+            '-n',
+            '-i', 'enp0s31f6',  # Attach to OAI CN interface
+            '-w', os.path.expanduser(f'~/{output_directory}/output.pcapng')
+        ]
+    pid = subprocess.Popen(tshark_cmd)
+    return pid
 
 # Stop tshark measurement
 def stop_tshark(tshark_process):
-    tshark_process.terminate()
+    tshark_process.kill()
     tshark_process.wait()
     logging.info("Tshark measurement stopped.")
