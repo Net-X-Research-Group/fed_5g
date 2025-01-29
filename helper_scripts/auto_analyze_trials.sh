997e59a9a6be873d676240ebe08c14745a0fbcdf
hayekr <hayekr2@gmail.com>
2025-01-29 12:52:26 -0600

    Add time extractor to automation

--- helper_scripts/auto_analyze_trials.sh
+++ helper_scripts/auto_analyze_trials.sh
@@ -6,6 +6,7 @@ source /home/rhayek/fed_5g/venv/bin/activate
 ANALYZER_SCRIPT="/home/rhayek/fed_5g/util/wireshark_comms_analyzer.py"
 COMMS_PLOTTER_SCRIPT="/home/rhayek/fed_5g/util/comms_metrics_agg.py"
 ML_PLOTTER_SCRIPT="/home/rhayek/fed_5g/util/plotter.py"
+TIME_EXTRACTOR="/home/rhayek/fed_5g/util/elapsed_extract.py"
 
 # Loop through all subdirectories in the current directory
 for dir in */; do
@@ -16,11 +17,13 @@ for dir in */; do
         cd "$dir" || { echo "Failed to change directory to $dir"; exit 1; }
         pwd
         # Run the Python script
-        echo "Running wireshark analyzer"
+        echo "Running wireshark analyzer..."
         python "$ANALYZER_SCRIPT" -p output.pcapng -c ../config.yml
-        echo "Running comms metrics plotter"
+        echo "Extracting elapsed times..."
+	python "$TIME_EXTRACTOR"
+	echo "Running comms metrics plotter..."
         python "$COMMS_PLOTTER_SCRIPT"
-        echo "Running ML metrics plotter"
+        echo "Running ML metrics plotter..."
         python "$ML_PLOTTER_SCRIPT" -f agg_metrics.json
         
         # Change back to parent directory
