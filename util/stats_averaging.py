38ca2e83b16a14fd8e3db47440451dc06e61a7db
Kayla Comer <kmcomer2002@gmail.com>
2025-03-16 23:07:16 -0500

    create new file

---
+++ util/stats_averaging.py
@@ -0,0 +1,16 @@
+import json
+import re
+from os import listdir
+from os.path import join, isdir
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    input_dir = input("Enter the path to the directory containing the trials: ")
+    main(input_dir)
