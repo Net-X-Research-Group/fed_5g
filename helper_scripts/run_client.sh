26d7a58dd6ac145a06b35f57f9538148ee13be17
Robert Hayek <hayekr2@gmail.com>
2025-04-17 08:24:24 -0500

    fork ue monitor

--- helper_scripts/run_client.sh
+++ helper_scripts/run_client.sh
@@ -9,4 +9,10 @@ CID=$2
 SERVER_IP=$3
 
 source "${HOME}"/fl_venv/bin/activate
+
+python "${HOME}"/fed_5g/util/ue_phy_metrics.csv &
+PYTHON_PID=$!
+
+trap "kill $PYTHON_PID 2>/dev/null" EXIT INT TERM
+
 flower-supernode --insecure --superlink="${SERVER_IP}:9092" --node-config="dataset='~/node_datasets/cifar10_${DISTRIBUTION}' cid=${CID}"
