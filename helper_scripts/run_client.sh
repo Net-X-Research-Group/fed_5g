f6b51229552930efdeb4ddb84c5a1b929658d581
Robert Hayek <hayekr2@gmail.com>
2025-04-22 16:14:41 -0500

    add debugging

--- helper_scripts/run_client.sh
+++ helper_scripts/run_client.sh
@@ -12,7 +12,7 @@ source "${HOME}"/fl_venv/bin/activate
 
 python "${HOME}"/fed_5g/util/ue_phy_metrics.py &
 PYTHON_PID=$!
-
+echo "Python process started with PID: $PYTHON_PID"
 trap "kill $PYTHON_PID 2>/dev/null" EXIT INT TERM
 
 flower-supernode --insecure --superlink="${SERVER_IP}:9092" --node-config="dataset='~/node_datasets/cifar10_${DISTRIBUTION}' cid=${CID}"
