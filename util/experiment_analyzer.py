b0b3fe0b7e891db26f701fbc3c8b28de659bb012
Robert Hayek <hayekr2@gmail.com>
2025-05-16 13:12:21 -0500

    add classes, parser exporting csv

--- util/experiment_analyzer.py
+++ util/experiment_analyzer.py
@@ -1,6 +1,8 @@
+import concurrent.futures
 import json
+import os
 import re
-from os import listdir
+from os import listdir, makedirs
 from os.path import join, isdir
 
 import matplotlib.pyplot as plt
@@ -19,100 +21,210 @@ plt.rcParams.update({
     })
 
 
-def _read_json(file):
-    with open(file, 'r') as f:
-        raw = json.load(f)
-    return raw
-
-
-def export_metrics_to_csv(data, output_dir):
+class Parser:
     """
-    Export each metric to a separate CSV file where columns are CIDs and rows are rounds.
-
-    Args:
-        data (dict): Nested dictionary with structure {Round#: {CID: {metrics}}}
-        output_dir (str): Directory to save the CSV files
+    Given a configuration directory, grab and combine the trials, output to configuration directory.
     """
-    # Get all metrics from the first round and first CID
-    first_round = list(data.keys())[0]
-    first_cid = list(data[first_round].keys())[0]
-    metrics = list(data[first_round][first_cid].keys())
+    def __init__(self, config_dir):
+        self.config_dir = config_dir
 
-    # Get all unique CIDs
-    cids = set()
-    for round_data in data.values():
-        cids.update(round_data.keys())
-    cids = sorted(list(cids))
+    @staticmethod
+    def read_json(file):
+        with open(file, 'r') as f:
+            raw = json.load(f)
+        return raw
 
-    # Get all rounds
-    rounds = sorted(list(data.keys()), key=int)
+    def process(self):
+        """
+        Process the configuration directory, parse the data, and export to CSV.
 
-    ''' Just repeat the code from the following for loop (for metric in metrics) to inelegantly calculate round time'''
-    round_time_df = pd.DataFrame(index=rounds)
-    for cid in cids:
-        cid_values = []
-        for round_num in rounds:
-            if cid in data[round_num]:
-                value = 0
-                for metric in list(data[first_round][first_cid].keys()):
-                    if 'time' in metric and 'start' not in metric and 'end' not in metric:
-                        # cid_values.append(data[round_num][cid][metric])
-                        value += data[round_num][cid][metric]
-                cid_values.append(value)
-            else:
-                cid_values.append(None)  # Handle missing data
-        round_time_df[f'CID_{cid}'] = cid_values
-    round_time_df.index.name = 'Round'
-    # Save to CSV
-    filename = f"round_time.csv"
-    filepath = join(output_dir, filename)
-    round_time_df.to_csv(filepath, index=False)
-    if VERBOSITY == 2:
-        print(f"Saved {str(filepath)}")
-    ''' End of repeated code section (repeats following code for one new metric)'''
+        Args:
+            data (dict): Nested dictionary with structure {Round#: {CID: {metrics}}}
+            output_dir (str): Directory to save the CSV files
+        """
+        trial_dirs = [d for d in listdir(self.config_dir) if isdir(join(self.config_dir, d))]
 
-    comm_round_time = round_time_df.max(axis=1, numeric_only=True)
-    comm_round_time.to_csv(join(output_dir, 'comm_' + filename), index=False)
-    # df = pd.concat(metrics_dict.values(), axis=1).T.groupby(level=0).mean().T #here
-    # data[display_name][metric] = pd.read_csv(join(str(result_path), filename)).mean(axis=1)
+        for trial_dir in trial_dirs:
+            trial_path = join(self.config_dir, trial_dir)
+
+            individual_raw_data = self.read_json(join(trial_path, "individual_metrics.json"))
+
+            # Create output directory if it doesn't exist
+            try:
+                makedirs(join(trial_path, 'output'))
+            except FileExistsError:
+                pass
+
+            # Export before doing anything
+            self.export_individual(individual_raw_data, output_dir=join(trial_path, 'output'))
+
+            self.compile_latency(trial_path, output_dir=join(trial_path, 'output'))
+            self.parse_rfsts(trial_path, output_dir=join(trial_path, 'output'))
+
+            # Compile the trials into one output dir in each configuration.
+            self.aggregate_trials(trial_dirs)
+
+    def aggregate_trials(self, trial_dirs):
+        output_path = join(self.config_dir, 'output')
+        makedirs(output_path, exist_ok=True)
+
+        trial_output_dirs = [join(self.config_dir, d, 'output') for d in trial_dirs]
+
+        csv_files = [f for f in listdir(trial_output_dirs[0]) if f.endswith('.csv')]
+
+        for file in csv_files:
+            dfs = []
+            for trial in trial_output_dirs:
+                path = join(trial, file)
+                if os.path.exists(path):
+                    dfs.append(pd.read_csv(path))
+            if not dfs:
+                continue
+
+            all_columns = sorted(set(col for df in dfs for col in df.columns))
+            aligned_dfs = [df.reindex(columns=all_columns) for df in dfs]
 
 
-    # For each metric, create a DataFrame and save to CSV
-    for metric in metrics:
-        # Create empty DataFrame with rounds as index
-        df = pd.DataFrame(index=rounds)
+            if any(metric in file for metric in ['rsrp', 'rsrq', 'rssi']):
+                vstack_df = pd.concat(aligned_dfs, axis=0, ignore_index=True)
+                vstack_df.to_csv(join(output_path, f'vstack_{file}'), index=False)
 
-        # Fill in data for each CID
+
+    @staticmethod
+    def export_individual(data, output_dir):
+        """
+        Export each metric to a separate CSV file where columns are CIDs and rows are rounds.
+
+        Args:
+            data (dict): Nested dictionary with structure {Round#: {CID: {metrics}}}
+            output_dir (str): Directory to save the CSV files
+        """
+
+        # Get all metrics from the first round and first CID
+        first_round = list(data.keys())[0]
+        first_cid = list(data[first_round].keys())[0]
+        metrics = list(data[first_round][first_cid].keys())
+
+        # Get all unique CIDs
+        cids = set()
+        for round_data in data.values():
+            cids.update(round_data.keys())
+        cids = sorted(list(cids))
+
+        # Get all rounds
+        rounds = sorted(list(data.keys()), key=int)
+
+        ''' Just repeat the code from the following for loop (for metric in metrics) to inelegantly calculate round time'''
+        round_time_df = pd.DataFrame(index=rounds)
         for cid in cids:
             cid_values = []
             for round_num in rounds:
                 if cid in data[round_num]:
-                    cid_values.append(data[round_num][cid][metric])
+                    value = 0
+                    for metric in list(data[first_round][first_cid].keys()):
+                        if 'time' in metric and 'start' not in metric and 'end' not in metric:
+                            # cid_values.append(data[round_num][cid][metric])
+                            value += data[round_num][cid][metric]
+                    cid_values.append(value)
                 else:
                     cid_values.append(None)  # Handle missing data
-            df[f'CID_{cid}'] = cid_values
-        df.index.name = 'Round'
+            round_time_df[f'CID_{cid}'] = cid_values
+        round_time_df.index.name = 'Round'
         # Save to CSV
-        filename = f"{metric}.csv"
+        filename = f"round_time.csv"
         filepath = join(output_dir, filename)
-        df.to_csv(filepath, index=False)
-        if VERBOSITY == 1:
-            print(f"Saved {str(filepath)}")
+        round_time_df.to_csv(filepath, index=False)
+
+        ''' End of repeated code section (repeats following code for one new metric)'''
+
+        comm_round_time = round_time_df.max(axis=1, numeric_only=True)
+        comm_round_time.to_csv(join(output_dir, 'comm_' + filename), index=False)
+        # df = pd.concat(metrics_dict.values(), axis=1).T.groupby(level=0).mean().T #here
+        # data[display_name][metric] = pd.read_csv(join(str(result_path), filename)).mean(axis=1)
+
+        # For each metric, create a DataFrame and save to CSV
+        for metric in metrics:
+            # Create empty DataFrame with rounds as index
+            df = pd.DataFrame(index=rounds)
+
+            # Fill in data for each CID
+            for cid in cids:
+                cid_values = []
+                for round_num in rounds:
+                    if cid in data[round_num]:
+                        cid_values.append(data[round_num][cid][metric])
+                    else:
+                        cid_values.append(None)  # Handle missing data
+                df[f'CID_{cid}'] = cid_values
+            df.index.name = 'Round'
+            # Save to CSV
+            filename = f"{metric}.csv"
+            filepath = join(output_dir, filename)
+            df.to_csv(filepath, index=False)
+
+    @staticmethod
+    def compile_latency(path, output_dir):
+        files = [f for f in listdir(path) if f.startswith('latency')]
+        results = {}
+        for file in files:
+            cid = re.search(r'latency_\d+_CID(\d+)', file)
+            if not cid:
+                continue
+            cid = cid.group(1)
+            results[f'CID_{cid}'] = pd.read_csv(join(path, file), names=['Round', 'Downlink', 'Uplink'])
+
+        uplink_df = pd.DataFrame()
+        downlink_df = pd.DataFrame()
+
+        for cid, df in results.items():
+            uplink_df[cid] = df['Uplink']
+            downlink_df[cid] = df['Downlink']
+
+        uplink_df.to_csv(join(output_dir, 'latency_uplink.csv'), index=False)
+        downlink_df.to_csv(join(output_dir, 'latency_downlink.csv'), index=False)
+
+    @staticmethod
+    def parse_rfsts(path, output_dir):
+        files = [f for f in listdir(path) if f.startswith('rfsts')]
+        results = {}
+        for file in files:
+            cid = re.search(r'rfsts.csv_\d+_CID(\d+)', file)
+            if not cid:
+                continue
+            cid = cid.group(1)
+            results[f'CID_{cid}'] = pd.read_csv(join(path, file))
+
+        rsrq_df = pd.DataFrame()
+        rssi_df = pd.DataFrame()
+        rsrp_df = pd.DataFrame()
+        for cid, df in results.items():
+            # Include timestamp
+            rsrq_df['Timestamp'] = df['timestamp']
+            rssi_df['Timestamp'] = df['timestamp']
+            rsrp_df['Timestamp'] = df['timestamp']
+            rsrq_df[cid] = df['rsrq']
+            rssi_df[cid] = df['rssi']
+            rsrp_df[cid] = df['rsrp']
+        rsrq_df.to_csv(join(output_dir, 'rsrq.csv'), index=False)
+        rssi_df.to_csv(join(output_dir, 'rssi.csv'), index=False)
+        rsrp_df.to_csv(join(output_dir, 'rsrp.csv'), index=False)
 
 
-def process_trial_latency(path: str) -> dict:
-    """
-    Process latency data captured by patched flower version.
-    """
-    files = [f for f in listdir(path) if f.startswith('latency')]
-    results = {}
-    for file in files:
-        cid = re.search(r'latency_\d+_CID(\d+)', file)
-        if not cid:
-            continue
-        cid = cid.group(1)
-        results[f'CID_{cid}'] = pd.read_csv(join(path, file), names=['Round', 'Downlink', 'Uplink'])
-    return results
+class Aggregator:
+    def __init__(self, config_dir):
+        self.config_dir = config_dir
+
+    def process(self):
+        # Implement the aggregation logic here
+        pass
+
+
+class Vizualizer:
+    def __init__(self, config_dir):
+        self.config_dir = config_dir
+
+    def proces(self):
+        pass
 
 
 def _aggregate_metrics(metrics_dict: dict, output_path: str, name: str) -> pd.DataFrame:
@@ -137,8 +249,6 @@ def _aggregate_ml_metrics(metrics_dict: dict, output_path: str, name: str) -> li
         # Drop all but 'avg'
         agg_dfs.append(df[['avg']])
     pd.concat(agg_dfs, axis=1).to_csv(join(output_path, f'avg_{name}.csv'), index=False)  # Concat and save as csv
-    if VERBOSITY >= 1:
-        print(f'Saved {output_path}/avg_{name}.csv')
     return agg_dfs
 
 
@@ -727,31 +837,16 @@ def plot_configurations(input_path, experiment_dir):
         except Exception as e:
             print(f"Error processing {metric}: {e}")
 
-def main(input_path: str) -> None:
-    experiment_dirs = [d for d in listdir(input_path) if isdir(join(input_path, d))]
+def main() -> None:
+    input_dir = '/Users/roberthayek/Documents/git_repos/fed_5g/5G_Tests'
 
-    if AGGREGATE_TRIAL_DATA:
-        retrieve_configuration_metrics(input_path, experiment_dirs)
+    configuration_dirs = [d for d in listdir(input_dir) if isdir(join(input_dir, d))]
 
-    if PLOT_EXPERIMENT_DATA:
-        plot_configurations(input_path, experiment_dirs)
-
+    # Create a thread for each configuration for speedup
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        parsers = [Parser(join(input_dir, config_dir)) for config_dir in configuration_dirs]
+        futures = [executor.submit(parser.process) for parser in parsers]
+        concurrent.futures.wait(futures)
 
 if __name__ == '__main__':
-    ML_CONFIDENCE_BANDS = True
-    PLOT_ROUNDS_INSTEAD_OF_TIME = False
-
-    ''' level of print statements (higher level -> more print statements)
-    - when csv files are created and saved from trial jsons (level 2)
-    - csv files created for aggregated metrics (level 1)
-    - errors always printed
-    '''
-    VERBOSITY = 1
-
-    PLOT_EXPERIMENT_DATA = True # whether we should plot overview figures of all configurations
-    AGGREGATE_TRIAL_DATA = False # whether we should go into each configuration (ex. 3Node_Ethernet_IID) and aggregate data from all trials (directories named with run IDs)
-    PLOT_TRIAL_DATA = False # whether we should go into each configuration (ex. 3Node_Ethernet_IID) and plot time, latency data by CID; ML training and validation data. sub-condition of AGGREGATE_ALL_TRIAL_DATA
-    IMPORT_ALL_TRIAL_DATA = True # whether we should go into each trial (within in a configuration) and export the json data to CSV files. sub-condition of AGGREGATE_ALL_TRIAL_DATA
-
-    input_dir = input("Enter the path to the directory containing the trials: ")
-    main(input_dir)
+    main()
