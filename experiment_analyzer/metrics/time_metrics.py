8cb6a7a8e2cc1da28aad693aec8c5af17bde2812
Kayla Comer <kmcomer2002@gmail.com>
2025-08-04 16:38:20 -0500

    fix aggregate_across_trials

--- experiment_analyzer/metrics/time_metrics.py
+++ experiment_analyzer/metrics/time_metrics.py
@@ -9,8 +9,11 @@ import matplotlib.ticker as ticker
 import seaborn as sns
 from plotting_util import setup_plotting, remove_underscore
 
+from data_models import Trial, Configuration
+
 import logging_setup
 logger = logging_setup.setup_logging('debug') # Custom logging setup for the module
+plt.set_loglevel('WARNING')
 
 class TimeMetric(Metric):
     def __init__(self, metric_name: str) -> None:
@@ -41,11 +44,17 @@ class TimeMetric(Metric):
 
         return individual, aggregated
 
-    def aggregate_across_trials(self, trials: List[pd.DataFrame], config_output_path: Path) -> Optional[pd.DataFrame]:
-        aggregated = pd.concat(trials, axis=1).T.groupby(level=0).mean().T
-        aggregated.to_csv(config_output_path / f'aggregated_{self.name}.csv', index=False)
+    def aggregate_across_trials(self, configuration: Configuration, trials: List[pd.DataFrame]) -> Optional[pd.DataFrame]:
+        output_dir = configuration.get_output_path()
+        
+        individual_dfs = [trial[0] for trial in trials]
+        aggregated_dfs = [trial[1] for trial in trials]
 
-        return aggregated
+        aggregated = pd.concat(individual_dfs, axis=1).T.groupby(level=0).mean().T
+        aggregated_dfs_aggregated = pd.concat(aggregated_dfs, axis=1).T.groupby(level=0).mean().T
+        aggregated.to_csv(output_dir / f'aggregated_{self.name}.csv', index=False)
+
+        return aggregated, aggregated_dfs_aggregated
 
     def aggregate_across_configs(self, config_dfs: Dict[str, pd.DataFrame], experiment_output_path: Path) -> Optional[pd.DataFrame]:
         configurations = config_dfs.keys()
@@ -87,7 +96,80 @@ class TimeMetric(Metric):
         return
 
     def visualize_across_configs(self, dfs: Dict[str, pd.DataFrame], output_path_str: str) -> None:
-        pass
+        setup_plotting()
+
+        # plot_data = []
+        # sorted_configs = sorted(dfs.keys())
+
+        # for config_name in sorted_configs:
+        #     uplink_df, downlink_df = dfs[config_name]
+        #     for _, uplink_value in uplink_df.items():
+        #         plot_data.append({
+        #             'Configuration': config_name,
+        #             'Direction': 'Uplink',
+        #             'Time (s)': uplink_value
+        #         })
+        #     for _, downlink_value in downlink_df.items():
+        #         plot_data.append({
+        #             'Configuration': config_name,
+        #             'Direction': 'Downlink',
+        #             'Time (s)': downlink_value
+        #         })
+
+        # plot_df = pd.DataFrame(plot_data)
+        # plt.figure(figsize=(12, 6))
+        # sns.boxplot(data=plot_df, x='Configuration', y='Time (s)', hue='Direction', showfliers=False)
+        # plt.grid(True, alpha=0.3)
+        # plt.savefig(output_path_str / f'{self.name}_combined_box_plot.png')
+        # plt.close()
+
+    #     fig, axs = plt.subplots(num_configurations, 1, figsize=(10, 4 * num_configurations))
+
+    # # Handle single column case
+    # if num_configurations == 1:
+    #     axs = [axs]
+
+    # # Main Histogram
+    # for idx, col in enumerate(data.columns):
+    #     axs[idx].hist(data[col], alpha=0.75, color='blue')
+    #     #axs[idx].set_title(f'{col.replace("_", " ")} Distribution')
+    #     axs[idx].set_xlabel('Time (s)')
+    #     axs[idx].set_ylabel('Frequency')
+    #     axs[idx].grid(True, alpha=0.3)
+    # #plt.suptitle(f'{name} Distribution by Configuration')
+    # plt.tight_layout()
+    # plt.savefig(join(output_dir, f'{name.lower().replace(" ", "_")}_histograms'))
+    # # plt.show()
+    # plt.close()
+
+    # # Violin plots
+    # plt.figure(figsize=(10, 6))
+    # plot_data = pd.melt(data, var_name='Configuration', value_name='Time (s)')
+    # sns.violinplot(data=plot_data, x='Configuration', y='Time (s)', inner='quartile', density_norm="area", common_norm=True)
+    # #plt.title(f'{name} Distribution by Configuration')
+    # plt.grid(True, alpha=0.3)
+    # plt.tight_layout()
+    # plt.savefig(join(output_dir, f'{name.lower().replace(" ", "_")}_violin'))
+    # # plt.show()
+    # plt.close()
+
+    # # Overlay Histograms
+    # fig, ax = plt.subplots(figsize=(10, 6))
+    # for col in data.columns:
+    #     ax.hist(data[col], alpha=0.75, label=col.replace("_", " "))
+    # #ax.set_title(f'{name} Distribution')
+    # ax.set_xlabel('Time (s)')
+    # ax.set_ylabel('Frequency')
+    # ax.legend()
+    # ax.grid(True, alpha=0.3)
+    # plt.tight_layout()
+    # plt.savefig(join(output_dir, f'{name.lower().replace(" ", "_")}_overlay_histogram'))
+    # # plt.show()
+    # plt.close()
+
+        # logger.info(f'{self._metric_name} Metric: Outputted combined box plot for configurations.')
+
+        return
 
     def visualize_single_config(self, df: pd.DataFrame, output_path_str: str) -> None:
         pass
