3bb069da10c9e86d2884b1c8bbe165ff7d8db607
Robert Hayek <hayekr2@gmail.com>
2025-09-30 00:59:44 -0500

    Update run_trials.sh to include deployment parameter and streamline execution

--- helper_scripts/run_trials.sh
+++ helper_scripts/run_trials.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <NUM OF NODES> <DISTRIBUTION>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <NUM OF NODES> <DISTRIBUTION> <DEPLOYMENT>"
     exit 1
 fi
 
 N=$1
 DIST=$2
 
-for i in $(seq 1 20);
-do
 	./dataset_distributor.sh "${N}" "${DIST}"
-	mv cifar10_"${N}"_partitions_"${DIST}".png cifar10_"${N}"_partitions_"${DIST}"_trial_"${i}".png
 	sleep 2
-	flwr run "$HOME"/fed_5g/flwr-application --stream
+	flwr run "$HOME"/fed_5g/flwr-application "${DEPLOYMENT}" --stream
 	sleep 2
 	#kill -9 $(pgrep tshark)
   ./transfer_latency_measurements.sh "${N}"
-done
 
 flwr ls "$HOME"/fed_5g/flwr-application --format json > "${HOME}"/trials.json
