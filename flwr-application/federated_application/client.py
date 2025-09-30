9bfbadae107cf021fe58e0d67ec61d7803d68a1e
Robert Hayek <hayekr2@gmail.com>
2025-09-30 18:21:41 -0500

    Update client app

--- flwr-application/federated_application/client.py
+++ flwr-application/federated_application/client.py
@@ -1,122 +1,72 @@
-import logging
-import warnings
-from datetime import datetime
 from os import path
 
 import torch
-import wandb
-from flwr.client import NumPyClient, ClientApp
-from flwr.client.mod import message_size_mod
-from flwr.common import Context
+from flwr.client import ClientApp
+from flwr.common import Context, Message, ArrayRecord, MetricRecord, RecordDict
 
 from federated_application.models import ModelWrapper
-from federated_application.mods import comm_time_mod, phy_layer_measurement_mod
-from federated_application.task import (
-    get_weights,
-    set_weights,
-    load_dataset,
-    train,
-    test
-)
+from federated_application.task import load_dataset, train_fn, test_fn
 
-warnings.filterwarnings("ignore", category=UserWarning)
+app = ClientApp()
 
-# Set up logging
-logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-                    )
-logger = logging.getLogger(__name__)
+@app.train()
+def train(msg: Message, context: Context):
+    # Load the model and initialize it with the received weights
+    model = ModelWrapper.create_model(model=context.run_config['model'], num_classes=10)
+    model = torch.jit.script(model)  # Enable JIT to reduce python overhead
+    model.load_state_dict(msg.content['arrays'].to_torch_state_dict())
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-PROJECT_NAME = "Pytorch-5G-FLWR-CIFAR10"
+    # Load the data
+    cid: str = context.node_config['cid']
+    batch_size: int = context.run_config['batch_size']
+    dataset_path: str = path.expanduser(f"{context.node_config['dataset']}_part_{cid}")
+    trainloader, _ = load_dataset(dataset_path, batch_size)
 
+    # Call the training function
+    train_loss, train_time = train_fn(
+        net=model,
+        trainloader=trainloader,
+        epochs=context.run_config['epochs'],
+        learning_rate=context.run_config['learning_rate'],
+        momentum=context.run_config['momentum'],
+        weight_decay=context.run_config['weight_decay'],
+        device=device
+    )
 
-class FlowerClient(NumPyClient):
-    def __init__(self, trainloader, valloader, local_epochs, learning_rate, momentum, weight_decay, enable_wandb,
-                 run_config) -> None:
-        self.net = ModelWrapper.create_model(model=run_config['model'], num_classes=10)
-        self.net = torch.jit.script(self.net)  # Enable JIT to reduce python overhead
-        self.trainloader = trainloader
-        self.valloader = valloader
-        self.local_epochs = local_epochs
-        self.learning_rate = learning_rate
-        self.momentum = momentum
-        self.weight_decay = weight_decay
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.net.to(self.device)
-        self.enable_wandb = enable_wandb
-        self.wandb_config = run_config
+    model_record = ArrayRecord(model.state_dict())
+    metrics = {
+        'train_loss': train_loss,
+        'train_time': train_time,
+    }
 
-        # Login to wandb
-        if enable_wandb:
-            logger.info('Enabling wandb...')
-            wandb.login(key=run_config['wandb_api_key'])
-            self.wandb_config.pop('wandb_api_key')
+    metric_record = MetricRecord(metrics)
+    content = RecordDict({'arrays': model_record, 'metrics': metric_record})
+    return Message(content=content, reply_to=msg)
 
-            # Initialize the wandb project
-            self._init_wandb_project()
+@app.evaluate()
+def evaluate(msg: Message, context: Context):
+    # Load the model and initialize it with the received weights
+    model = ModelWrapper.create_model(model=context.run_config['model'], num_classes=10)
+    model = torch.jit.script(model)  # Enable JIT to reduce python overhead
+    model.load_state_dict(msg.content['arrays'].to_torch_state_dict())
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-    def _init_wandb_project(self):
-        wandb.init(project=PROJECT_NAME,
-                   group=str(self.wandb_config['run_id']),
-                   id=f'{self.wandb_config["run_id"]}-{self.wandb_config["cid"]}',
-                   name=f'{datetime.now().strftime("%Y-%m-%d/%H-%M-%S")}_CID-{self.wandb_config["cid"]}',
-                   resume='allow',
-                   reinit=True,
-                   config=dict(self.wandb_config))
+    # Load the data
+    cid: str = context.node_config['cid']
+    batch_size: int = context.run_config['batch_size']
+    dataset_path: str = path.expanduser(f"{context.node_config['dataset']}_part_{cid}")
+    _, valloader = load_dataset(dataset_path, batch_size)
+    eval_loss, eval_acc, eval_time = test_fn(model, valloader, device)
 
-    def fit(self, parameters, config) -> tuple:
-        """Train the client model on the local training dataset"""
-        set_weights(self.net, parameters)
-        results = train(
-            net=self.net,
-            trainloader=self.trainloader,
-            valloader=self.valloader,
-            epochs=self.local_epochs,
-            learning_rate=self.learning_rate,
-            momentum=self.momentum,
-            weight_decay=self.weight_decay,
-            device=self.device
-        )
-        results['cid'] = self.wandb_config['cid']
-        logger.info(f"Training complete. Elapsed time: {results['training_time']}")
-        if self.enable_wandb:
-            wandb.log(results)
-        return get_weights(self.net), len(self.trainloader.dataset), results
+    metrics = {
+        'eval_loss': eval_loss,
+        'eval_acc': eval_acc,
+        'eval_time': eval_time,
+    }
 
-    def evaluate(self, parameters, config):
-        """Evaluate the client model on the local validation dataset"""
-        set_weights(self.net, parameters)
-        loss, accuracy, eval_time = test(net=self.net, testloader=self.valloader, device=self.device)
-        metrics = {
-            'accuracy': accuracy,
-            'loss': loss
-        }
-        return loss, len(self.valloader.dataset), metrics
-
-
-def client_fn(context: Context):
-    cid = context.node_config['cid']
-    dataset_path = path.expanduser(f"{context.node_config['dataset']}_part_{cid}")
-    batch_size = context.run_config['batch_size']
-    local_epochs = context.run_config['local_epochs']
-    learning_rate = context.run_config['learning_rate']
-    momentum = context.run_config['momentum']
-    weight_decay = context.run_config['weight_decay']
-    trainloader, valloader = load_dataset(dataset_path=dataset_path, batch_size=batch_size)
-
-    # Set up the Config for wandb
-    config = context.run_config.copy()
-    config['cid'] = cid
-    config['run_id'] = context.run_id
-    enable_wandb = context.run_config['enable_client_wandb']
-    return FlowerClient(trainloader=trainloader,
-                        valloader=valloader,
-                        local_epochs=local_epochs,
-                        learning_rate=learning_rate,
-                        momentum=momentum,
-                        weight_decay=weight_decay,
-                        enable_wandb=enable_wandb,
-                        run_config=config).to_client()
-
-
-app = ClientApp(client_fn=client_fn, mods=[comm_time_mod, message_size_mod, phy_layer_measurement_mod])
+    metric_record = MetricRecord(metrics)
+    content = RecordDict({'metrics': metric_record})
+    return Message(content=content, reply_to=msg)
