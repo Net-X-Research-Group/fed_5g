6ff77cd3a28285e55c48628eb8751c6043d81581
Kayla Comer <kmcomer2002@gmail.com>
2026-02-18 16:33:23 -0600

    save data rate to csv file

--- helper_scripts/auto_iperf.sh
+++ helper_scripts/auto_iperf.sh
@@ -21,8 +21,13 @@ for ((CID=1;CID<=NUM_CLIENTS;CID++)); do
     ssh "$LOGIN" sudo nmcli r wwan off
 done
 
+echo "device,rate(Mbps)" > "iperf/${BW}_${TDD}_UL.csv"
+echo "device,rate(Mbps)" > "iperf/${BW}_${TDD}_DL.csv"
+
 for ((CID=1;CID<=NUM_CLIENTS;CID++)); do
     LOGIN=$DEVICE_NAME_PREFIX$CID@$IP_PREFIX${IP_SUFFIXES[$CID-1]}
+    echo
+    echo $LOGIN
     ssh "$LOGIN" sudo nmcli r wwan on
     ssh "$LOGIN" sudo nmcli connection up OAI
     ssh "$LOGIN" sudo route add -net 172.31.0.0/24 wwan0
@@ -30,11 +35,15 @@ for ((CID=1;CID<=NUM_CLIENTS;CID++)); do
     echo ip address: ${wwan}
     
     read -p "Start downlink test on receiver. Set protocol to TCP, bandwidth to 100 Gbps, and modify destination IP. Press Enter once ready to begin..."
-    ssh "$LOGIN" iperf -s -i 1 -B "$wwan" -t 120 > "iperf/${BW}_${TDD}_pi0${CID}_DL.txt"
-    tail -1 "iperf/${BW}_${TDD}_${LOGIN}_DL.txt"
+    ssh "$LOGIN" iperf -s -i 1 -fm -B "$wwan" -t 120 > "iperf/${BW}_${TDD}_pi0${CID}_DL.txt"
+    DL_RATE=$(tail -1 "iperf/${BW}_${TDD}_pi0${CID}_DL.txt" | grep -oE '[0-9.]+\s+Mbits/sec' | awk '{print $1}')
+    echo "DL Rate: ${DL_RATE} Mbits/sec"
+    echo "commnetpi0${CID},${DL_RATE}" >> "iperf/${BW}_${TDD}_DL.csv"
     
-    ssh "$LOGIN" iperf -t 120 -i 1 -fm -b 1000M -c 172.31.0.135 -B "$wwan" > "iperf/${BW}_${TDD}_pi${CID}_UL.txt"
-    tail -1 "iperf/${BW}_${TDD}_${LOGIN}_UL.txt"
+    ssh "$LOGIN" iperf -t 120 -i 1 -fm -b 1000M -c 172.31.0.135 -B "$wwan" > "iperf/${BW}_${TDD}_pi0${CID}_UL.txt"
+    UL_RATE=$(tail -1 "iperf/${BW}_${TDD}_pi0${CID}_UL.txt" | grep -oE '[0-9.]+\s+Mbits/sec' | awk '{print $1}')
+    echo "UL Rate: ${UL_RATE} Mbits/sec"
+    echo "commnetpi0${CID},${UL_RATE}" >> "iperf/${BW}_${TDD}_UL.csv"
     ssh "$LOGIN" sudo nmcli r wwan off
 done
 
