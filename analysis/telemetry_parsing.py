b07abca7381f4346ea7c668a61a35dfe5323d1ab
Kayla Comer <kmcomer2002@gmail.com>
2026-04-02 11:47:46 -0500

    support for iperf rnti disconnection merging

--- analysis/telemetry_parsing.py
+++ analysis/telemetry_parsing.py
@@ -121,44 +121,47 @@ def read_data_from_csvs(main_fp, secondary_fp, columns=None):
 
 
 def gather_metrics_by_rnti(savepath, metrics=None):
-    try:
-        phys_layer = savepath.joinpath('phys_layer')
-    except FileNotFoundError as exception:
-        print(f'phys_layer directory not found on savepath {savepath}. Exception: {exception}')
-        return
-    
     ue_dfs = {}
-    for file in phys_layer.iterdir():
+    for file in savepath.iterdir():
         filename = file.name
         if 'ue' in filename and 'common' not in filename:
             rnti = (filename.split('_')[1]).split('.')[0]
-            ue_dfs[rnti] = read_data_from_csvs(str(phys_layer)+'/common.csv', str(file), metrics)
+            ue_dfs[rnti] = read_data_from_csvs(str(savepath)+'/common.csv', str(file), metrics)
     
     return ue_dfs
 
 
-def combine_uedfs_by_rnti(ue_dfs, first, second, trial_path):
+def combine_uedfs_by_rnti(ue_dfs, first, second, trial_path, drop_timestamp=True):
     last_pt_first_rnti = ue_dfs[first]['segment'][-1]
     first_pt_second_rnti = ue_dfs[second]['segment'][0]
-    assert(last_pt_first_rnti < first_pt_second_rnti)
+    try:
+        assert(last_pt_first_rnti < first_pt_second_rnti)
+    except AssertionError:
+        print(f'Exception. Trying to merge non-consecutive physical layer data: {first}\'s first point is {first_pt_second_rnti}. {second}\'s last point is {last_pt_first_rnti}')
+        raise
 
     new_name = first+'-'+second
     print(f'Merging {new_name} (from {first} and {second})')
     ue_dfs[new_name] = pl.concat([ue_dfs[first],ue_dfs[second]], how="vertical_relaxed")
-    ue_dfs[new_name].drop('timestamp').write_csv(f'{trial_path}/phys_layer/ue_{new_name}.csv')
-    os.remove(f'{trial_path}/phys_layer/ue_{first}.csv')
-    os.remove(f'{trial_path}/phys_layer/ue_{second}.csv')
+    if drop_timestamp:
+        ue_dfs[new_name].drop('timestamp')
+    ue_dfs[new_name].write_csv(f'{trial_path}/ue_{new_name}.csv')
+    os.remove(f'{trial_path}/ue_{first}.csv')
+    os.remove(f'{trial_path}/ue_{second}.csv')
     del ue_dfs[first]
     del ue_dfs[second]
     return new_name
 
 
-def merge_uedfs_single_disconnect(savepath): # Combination by only looking at whether one UE dropped and another joined at one time. Will not combine if multiple possible pairs (simultaneous disconnection)
-    try:
-        ue_dfs = gather_metrics_by_rnti(savepath)
-    except FileNotFoundError as exception:
-        print(f'phys_layer directory not found on savepath {savepath}. Exception: {exception}')
-        return
+def merge_uedfs_single_disconnect(savepath, drop_timestamp=True, iperf=False): # Combination by only looking at whether one UE dropped and another joined at one time. Will not combine if multiple possible pairs (simultaneous disconnection)
+    if iperf:
+        ue_dfs = {(file.name.split('_')[1]).split('.')[0] : pl.read_csv(savepath / file.name, try_parse_dates=True) for file in savepath.iterdir() if 'ue' in file.name}
+    else:
+        try:
+            ue_dfs = gather_metrics_by_rnti(savepath)
+        except FileNotFoundError as exception:
+            print(f'physical layer data not found on savepath {savepath}. Exception: {exception}')
+            return
     
     update = True
     while update:
@@ -169,18 +172,18 @@ def merge_uedfs_single_disconnect(savepath): # Combination by only looking at wh
             if rnti not in ue_dfs: # may have been merged and no longer exist
                 rnti = [name for name in ue_dfs if rnti in name][0] # there should only be one match
             first_pt = ue_dfs[rnti]['segment'][0]
-            print(f'{rnti, first_pt, ue_dfs[rnti]['segment'][-1]}')
+            # print(f'{rnti, first_pt, ue_dfs[rnti]['segment'][-1]}')
             if first_pt != 0:
-                possible_pairs = [ue_df for ue_df in ue_dfs if ue_dfs[ue_df]['segment'][-1] <= first_pt]
+                possible_pairs = [ue_df for ue_df in ue_dfs if ue_dfs[ue_df]['segment'][-1] < first_pt]
                 if len(possible_pairs) == 1:
                     update=True
-                    rnti = combine_uedfs_by_rnti(ue_dfs, possible_pairs[0], rnti, savepath)
+                    rnti = combine_uedfs_by_rnti(ue_dfs, possible_pairs[0], rnti, savepath, drop_timestamp=drop_timestamp)
             last_pt = ue_dfs[rnti]['segment'][-1]
             if last_pt != last_overall:
-                possible_pairs = [ue_df for ue_df in ue_dfs if ue_dfs[ue_df]['segment'][0] >= last_pt] # TODO fix
+                possible_pairs = [ue_df for ue_df in ue_dfs if ue_dfs[ue_df]['segment'][0] > last_pt]
                 if len(possible_pairs) == 1:
                     update=True
-                    rnti = combine_uedfs_by_rnti(ue_dfs, rnti, possible_pairs[0], savepath)
+                    rnti = combine_uedfs_by_rnti(ue_dfs, rnti, possible_pairs[0], savepath, drop_timestamp=drop_timestamp)
 
 
 def get_runs_list(path, name, cols, start_col, end_col):
@@ -245,35 +248,39 @@ def sort_telemetry_into_iperf(runs, telemetry_df, run_dir, parser, file):
 
 
 def main():
-    # dir = '/Users/kmcomer/Documents/5G Experiment Data/Phys-layer-unparsed/'
+    dir = '/Users/kmcomer/Documents/5G Experiment Data/'
 
     # Parse phys layer data from iperf trials
-    # for f in Path(dir).iterdir():
-    #     if 'iperf' in f.name:
-    #         print(f'{f.name}')
-    #         for t in f.iterdir():
-    #             print(f'Processing {t.name}')
-    #             if '0_' in t.name:
-    #                 runs = get_runs_list(t, 'UL.csv', ['start', 'device', 'end'], 'start', 'end')
-    #                 parse(dir, t, sort_telemetry_into_iperf, runs)
-    #                 runs = get_runs_list(t, 'DL.csv', ['start', 'device', 'end'], 'start', 'end')
-    #                 parse(dir, t, sort_telemetry_into_iperf, runs)
+    for f in Path(dir).iterdir():
+        if 'iperf' in f.name and 'zip' not in f.name:
+            print(f'{f.name}')
+            for t in f.iterdir():
+                print(f'Processing {t.name}')
+                if '0_' in t.name:
+                    for s in t.iterdir():
+                        if s.is_dir():
+                            merge_uedfs_single_disconnect(s, drop_timestamp=False, iperf=True)
+                    # runs = get_runs_list(t, 'UL.csv', ['start', 'device', 'end'], 'start', 'end')
+                    # parse(dir, t, sort_telemetry_into_iperf, runs)
+                    # runs = get_runs_list(t, 'DL.csv', ['start', 'device', 'end'], 'start', 'end')
+                    # parse(dir, t, sort_telemetry_into_iperf, runs)
+                    
     
     # Parse phys layer data from FL experiments
     # runs = get_runs_list(dir, 'Runs', ['Run ID', 'Created At', 'Finished At'], 'Created At', 'Finished At')
     # parse(dir, dir, sort_telemetry_into_trials, runs)
 
-    # Combine RNTIs
-    trial_path = Path('/Users/kmcomer/Documents/5G Experiment Data/Phys-layer-unparsed/7222719212451226563_6N_20MHz_7-2_MIMO2x2_Dirichlet')
-    ue_dfs = gather_metrics_by_rnti(trial_path)
-    combine_uedfs_by_rnti(ue_dfs, 'abca', '105c', trial_path) # should return error (105c precedes abca)
+    # Combine RNTIs by name
+    # trial_path = Path('/Users/kmcomer/Documents/5G Experiment Data/Phys-layer-unparsed/7222719212451226563_6N_20MHz_7-2_MIMO2x2_Dirichlet')
+    # ue_dfs = gather_metrics_by_rnti(trial_path)
+    # combine_uedfs_by_rnti(ue_dfs, 'abca', '105c', trial_path) # should return error (105c precedes abca)
 
     for path in Path('/Users/kmcomer/Documents/5G Experiment Data/Phys-layer-unparsed').iterdir():
         if path.is_dir():
             if 'iperf' in str(path):
                 pass
             else:
-                merge_uedfs_single_disconnect(path)
+                merge_uedfs_single_disconnect(path / 'phys_layer')
 
 if __name__ == '__main__':
     main()
